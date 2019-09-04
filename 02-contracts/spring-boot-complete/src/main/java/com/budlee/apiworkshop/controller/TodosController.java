9a49fa8e8358dea1141fc67d677cb03381079607
MattyA <mat.auburn@gmail.com>
2019-09-04 15:02:07 +0100

    Contract added for PUT request as if a consumer

--- 02-contracts/spring-boot-complete/src/main/java/com/budlee/apiworkshop/controller/TodosController.java
+++ 02-contracts/spring-boot-complete/src/main/java/com/budlee/apiworkshop/controller/TodosController.java
@@ -1,10 +1,13 @@
 package com.budlee.apiworkshop.controller;
 
 import com.budlee.apiworkshop.model.Todo;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriBuilder;
 
@@ -29,9 +32,10 @@ public class TodosController {
     public ResponseEntity<Void> createNewTodo(@RequestBody Todo todo) {
         final var key = incrementKey.getAndIncrement();
         todosMap.put(key, todo);
-        final var uri = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(key).toUri();
+//        final var uri = ServletUriComponentsBuilder.fromCurrentRequest()
+//                .path("/{id}")
+//                .buildAndExpand(key).toUri();
+        final var uri = URI.create("/todos/" + key);
         return ResponseEntity
                 .created(uri)
                 .build();
@@ -42,6 +46,19 @@ public class TodosController {
         return todosMap.containsKey(id) ?
                 ResponseEntity.ok(todosMap.get(id)) :
                 ResponseEntity.notFound().build();
+    }
+
+    @PutMapping(value = "/todos/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateTodo(@PathVariable(value = "id") Integer id, @RequestBody Todo todo) {
+        new RestTemplate()
+                .exchange(
+                        RequestEntity.put(URI.create("http://localhost:8083/todos/1"))
+                                .contentType(MediaType.APPLICATION_JSON)
+                        .body(todo),ResponseEntity.class
+                );
+
+        todosMap.replace(id, todo);
+        return ResponseEntity.noContent().build();
     }
 
 
---
+++ 02-contracts/spring-boot-complete/src/test/java/com/budlee/apiworkshop/controller/TestTodosControllerShould.java
@@ -0,0 +1,56 @@
+package com.budlee.apiworkshop.controller;
+
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRuleMk2;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.RequestResponsePact;
+import com.budlee.apiworkshop.model.Todo;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.*;
+
+public class TestTodosControllerShould {
+
+    @Rule
+    public PactProviderRuleMk2 mockProvider =
+            new PactProviderRuleMk2("todo-api-producer-pact", "localhost", 8083, this);
+
+
+    @Pact(consumer = "todo-api-consumer-pact")
+    public RequestResponsePact beerNotOk(PactDslWithProvider builder) {
+        return builder.
+                given("")
+                .uponReceiving("A successful PUT operation to update a Todo")
+                .matchPath("/todos/1")
+                .method("PUT")
+                .headers("Content-Type", "application/json")
+                .body("{\"message\":\"Make all the beds\"}")
+                .willRespondWith()
+                .status(204)
+                .toPact();
+    }
+
+    TodosController controller = new TodosController();
+
+    @Before
+    public void setUp(){
+//        this.controller.createNewTodo(new Todo("Make the bed"));
+    }
+
+
+    @Test
+    @PactVerification
+    public void runTestBeer() {
+
+//        this.controller.createNewTodo(new Todo("Make the bed"));
+
+        //NO CONTENT
+        assertEquals(this.controller.updateTodo(1, new Todo("Make all the beds")).getStatusCodeValue(), 204);
+//        assertEquals(this.controller.getTodo(1).getBody(), new Todo("Make all the beds"));
+
+    }
+}
